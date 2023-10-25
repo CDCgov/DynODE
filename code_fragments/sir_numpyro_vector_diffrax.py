7ae0c9a87174fd77d1ce13c2c07b4fdd35c9afb7
Ariel Shurygin <39861882+arik-shurygin@users.noreply.github.com>
2023-10-25 11:46:53 -0700

    testing pre-commit stuff

--- code_fragments/sir_numpyro_vector_diffrax.py
+++ code_fragments/sir_numpyro_vector_diffrax.py
@@ -1,21 +1,17 @@
-from jax.experimental.ode import odeint
+import jax.config
 import jax.numpy as jnp
 import numpy as np
-from jax.random import PRNGKey
-import jax.config
-
 import numpyro
 import numpyro.distributions as dist
-from numpyro.distributions.transforms import AffineTransform
-
-from numpyro.infer import MCMC, NUTS, Predictive
-from numpyro.infer.reparam import TransformReparam
-
-from diffrax import diffeqsolve, ODETerm, SaveAt, Tsit5
+from diffrax import ODETerm, SaveAt, Tsit5, diffeqsolve
+from jax.experimental.ode import odeint
+from jax.random import PRNGKey
+from numpyro.infer import MCMC, NUTS
 
 # Use 4 cores
 numpyro.set_host_device_count(4)
 jax.config.update("jax_enable_x64", True)
+
 
 def sir_ode(state, _, parameters):
     # Unpack state
@@ -23,7 +19,7 @@ def sir_ode(state, _, parameters):
     beta, gamma, contact_matrix = parameters
     population = s + i + r
 
-    # Compute flows 
+    # Compute flows
     ds_to_i = beta * s / population * contact_matrix.dot(i)
     di_to_r = gamma * i
 
@@ -32,19 +28,23 @@ def sir_ode(state, _, parameters):
     di = ds_to_i - di_to_r
     dr = di_to_r
 
-    return (ds, di, dr) #jnp.stack([ds, di, dr])
+    return (ds, di, dr)  # jnp.stack([ds, di, dr])
 
 
 rng = np.random.default_rng(seed=867530)
 n_age_groups = 5
-target_population_fractions = np.random.uniform(size = n_age_groups)
-target_population_fractions = target_population_fractions / sum(target_population_fractions)
+target_population_fractions = np.random.uniform(size=n_age_groups)
+target_population_fractions = target_population_fractions / sum(
+    target_population_fractions
+)
 contact_matrix = np.random.rand(n_age_groups, n_age_groups)
 
 # External parameters
 r0 = 1.5
 infectious_period = 3.0
-population = 10000 * n_age_groups * target_population_fractions #np.array([.25, .75])
+population = (
+    10000 * n_age_groups * target_population_fractions
+)  # np.array([.25, .75])
 population_fractions = population / sum(population)
 initial_infections = 10.0
 # contact_matrix = np.array([[18,  3],
@@ -53,16 +53,22 @@ initial_infections = 10.0
 contact_matrix = contact_matrix / max(abs(np.linalg.eigvals(contact_matrix)))
 
 # Internal model parameters and state
-initial_state = (population - initial_infections * population_fractions, #s
-                 initial_infections * population_fractions, #i
-                 population_fractions) #r
+initial_state = (
+    population - initial_infections * population_fractions,  # s
+    initial_infections * population_fractions,  # i
+    population_fractions,
+)  # r
 beta = r0 / infectious_period
 gamma = 1 / infectious_period
 
 
 # Solve ODE
-solution = odeint(sir_ode, initial_state, jnp.linspace(0.0, 100.0, 101),
-                  [beta, gamma, contact_matrix])
+solution = odeint(
+    sir_ode,
+    initial_state,
+    jnp.linspace(0.0, 100.0, 101),
+    [beta, gamma, contact_matrix],
+)
 incidence = -np.diff(solution[0], axis=0)
 
 # Generate incidence sample
@@ -79,13 +85,13 @@ t1 = 100.0
 dt0 = 0.1
 saveat = SaveAt(ts=jnp.linspace(t0, t1, 101))
 
-from diffrax import diffeqsolve, ODETerm, SaveAt, Tsit5
 
 def model(times, incidence):
     # Parameters
-    initial_infections = numpyro.sample("initial_infections",
-                                        dist.Exponential(1.0))
-    
+    initial_infections = numpyro.sample(
+        "initial_infections", dist.Exponential(1.0)
+    )
+
     # reparam_config = {"r0": TransformReparam()}
     # with numpyro.handlers.reparam(config=reparam_config):
     #     r0 = numpyro.sample("r0",
@@ -94,13 +100,17 @@ def model(times, incidence):
     #                             AffineTransform(1.0, 1.0)))
     excess_r0 = numpyro.sample("excess_r0", dist.Exponential(1.0))
     r0 = numpyro.deterministic("r0", 1 + excess_r0)
-        
-    infectious_period = numpyro.sample("infectious_period", dist.Exponential(1.0))
+
+    infectious_period = numpyro.sample(
+        "infectious_period", dist.Exponential(1.0)
+    )
 
     # Transform parameters
-    initial_state = (population - initial_infections * population_fractions, #s
-                     initial_infections * population_fractions, #i
-                     population_fractions) #r
+    initial_state = (
+        population - initial_infections * population_fractions,  # s
+        initial_infections * population_fractions,  # i
+        population_fractions,
+    )  # r
     beta = r0 / infectious_period
     gamma = 1 / infectious_period
 
@@ -108,8 +118,16 @@ def model(times, incidence):
     # solution = odeint(sir_ode, initial_state, times,
     #                   [beta, gamma, contact_matrix])
     # model_incidence = -jnp.diff(solution[0], axis=0)
-    solution = diffeqsolve(term, solver, t0, t1, dt0, initial_state,
-                           args=(beta, gamma, contact_matrix), saveat=saveat)
+    solution = diffeqsolve(
+        term,
+        solver,
+        t0,
+        t1,
+        dt0,
+        initial_state,
+        args=(beta, gamma, contact_matrix),
+        saveat=saveat,
+    )
     model_incidence = -jnp.diff(solution.ys[0], axis=0)
 
     # Observed incidence
@@ -124,7 +142,7 @@ mcmc = MCMC(
     num_chains=4,
     progress_bar=True,
 )
-mcmc.run(PRNGKey(8675309),
-         times=np.linspace(0.0, 100.0, 101),
-         incidence=incidence)
+mcmc.run(
+    PRNGKey(8675309), times=np.linspace(0.0, 100.0, 101), incidence=incidence
+)
 mcmc.print_summary()
