4ca833d6505a0b3a7a6224b3403c1813712e8cfe
Ben Toh <tjk3@cdc.gov>
2023-12-20 17:58:03 +0000

    Update fake data generation and fitting to fit IHR
    - Also fit Negative Binomial

--- gen_fake_dat_and_fit.py
+++ gen_fake_dat_and_fit.py
@@ -3,6 +3,7 @@ import jax.config
 import jax.numpy as jnp
 import numpy as np
 import numpyro
+import matplotlib.pyplot as plt
 
 from config.config_base import ConfigBase
 from mechanistic_compartments import build_basic_mechanistic_model
@@ -16,28 +17,44 @@ from model_odes.seip_model import seip_ode  # , seirw_ode
 numpyro.set_host_device_count(4)
 jax.config.update("jax_enable_x64", True)
 
+# %%
 # True model
-cb = ConfigBase()
-# cb.STRAIN_SPECIFIC_R0 = jnp.array([1.5, 2.5, 3.5])
+cb = ConfigBase(
+    POP_SIZE=1e8,
+    INITIAL_INFECTIONS=1e6,
+    INTRODUCTION_PERCENTAGE=0.01,
+    INTRODUCTION_TIMES=[60],
+)
+cb.STRAIN_SPECIFIC_R0 = jnp.array([1.5, 2.5, 2.5], dtype=jnp.float32)  # R0s
 model = build_basic_mechanistic_model(cb)
-solution = model.run(seip_ode, tf=100, save=False)
+ihr = [0.002, 0.004, 0.008, 0.06]
+
+solution = model.run(seip_ode, tf=300, show=True, save=False)
 model_incidence = jnp.sum(solution.ys[3], axis=(2, 3, 4))
 model_incidence = jnp.diff(model_incidence, axis=0)
 rng = np.random.default_rng(seed=8675399)
-m = np.asarray(model_incidence)
-fake_obs = rng.poisson(m)
+m = np.asarray(model_incidence) * ihr
+k = 10.0
+p = k / (k + m)
+fake_obs = rng.negative_binomial(k, p)
+
+# %%
+fig, ax = plt.subplots(1)
+ax.plot(m, label=[1, 2, 3, 4])
+fig.legend()
+plt.show()
 
 # %%
 # Perform inference
 # Reducing max_tree_depth here to reduce fitting time
 # The new way of doing it shown below!
-
+model.MCMC_NUM_WARMUP = 100
+model.MCMC_NUM_SAMPLES = 100
+model.MCMC_NUM_CHAINS = 1
 model.infer(
     seip_ode,
     fake_obs,
-    sample_dist_dict={
-        "infectious_period": numpyro.distributions.HalfCauchy(1.0)
-    },
+    negbin=True,
 )
 
 # mcmc = MCMC(
