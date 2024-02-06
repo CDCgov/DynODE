1fca7492d67f09e7ee86d18fc5bec446204e1bff
Ariel Shurygin <uva5@cdc.gov>
2024-02-06 12:32:14 -0800

    adding testing suite

---
+++ tests/test_initializer.py
@@ -0,0 +1,58 @@
+import pytest
+from mechanistic_model.covid_initializer import CovidInitializer
+
+
+CONFIG_GLOBAL_PATH = "tests/test_config_global.json"
+INITIALIZER_CONFIG_PATH = "tests/test_config_initializer.json"
+
+initializer = CovidInitializer(INITIALIZER_CONFIG_PATH, CONFIG_GLOBAL_PATH)
+
+
+def test_invalid_paths_raise():
+    with pytest.raises(AssertionError):
+        CovidInitializer("random_broken_path", CONFIG_GLOBAL_PATH),
+    with pytest.raises(AssertionError):
+        CovidInitializer(INITIALIZER_CONFIG_PATH, "random_broken_path")
+
+    with pytest.raises(AssertionError):
+        CovidInitializer("random_broken_path", "random_broken_path2")
+
+
+def test_initial_state_returns():
+    assert hasattr(initializer, "INITIAL_STATE")
+    assert initializer.get_initial_state() is not None
+
+
+def test_num_compartments():
+    assert (
+        len(initializer.get_initial_state()) == 4
+    ), "initial state returned not 4 compartments"
+
+
+def test_initial_state_shape():
+    expected_shape_S = (
+        initializer.NUM_AGE_GROUPS,
+        2**initializer.NUM_STRAINS,
+        initializer.MAX_VAX_COUNT + 1,
+        initializer.NUM_WANING_COMPARTMENTS,
+    )
+
+    expected_shape_rest = (
+        initializer.NUM_AGE_GROUPS,
+        2**initializer.NUM_STRAINS,
+        initializer.MAX_VAX_COUNT + 1,
+        initializer.NUM_STRAINS,
+    )
+    s, e, i, c = initializer.get_initial_state()
+    assert (
+        s.shape == expected_shape_S
+    ), "Susceptible compartment shape incorrect"
+    assert (
+        e.shape == expected_shape_rest
+    ), "Exposed compartment shape incorrect"
+    assert (
+        i.shape == expected_shape_rest
+    ), "Infected compartment shape incorrect"
+    assert (
+        c.shape == expected_shape_rest
+    ), "Book-keeping cumulative compartment shape incorrect"
---
+++ tests/test_runner.py
@@ -0,0 +1,105 @@
+from mechanistic_model.mechanistic_runner import MechanisticRunner
+from config.config import Config
+import pytest
+import jax.numpy as jnp
+from model_odes.seip_model import seip_ode
+import json
+
+CONFIG_GLOBAL_PATH = "tests/test_config_global.json"
+RUNNER_CONFIG_PATH = "tests/test_config_runner.json"
+global_config = Config(CONFIG_GLOBAL_PATH)
+S_SHAPE = (
+    global_config.NUM_AGE_GROUPS,
+    2**global_config.NUM_STRAINS,
+    global_config.MAX_VAX_COUNT + 1,
+    global_config.NUM_WANING_COMPARTMENTS,
+)
+EIC_SHAPE = (
+    global_config.NUM_AGE_GROUPS,
+    2**global_config.NUM_STRAINS,
+    global_config.MAX_VAX_COUNT + 1,
+    global_config.NUM_STRAINS,
+)
+fake_initial_state = (
+    500 * jnp.zeros(S_SHAPE),  # S
+    jnp.zeros(EIC_SHAPE),  # E
+    1 * jnp.zeros(EIC_SHAPE),  # I
+    jnp.zeros(EIC_SHAPE),  # C
+)
+runner = MechanisticRunner(
+    fake_initial_state, seip_ode, RUNNER_CONFIG_PATH, CONFIG_GLOBAL_PATH
+)
+ODES = seip_ode
+
+
+def test_invalid_paths_raise():
+    with pytest.raises(AssertionError):
+        MechanisticRunner(
+            fake_initial_state,
+            seip_ode,
+            "random_broken_path",
+            CONFIG_GLOBAL_PATH,
+        ),
+    with pytest.raises(AssertionError):
+        MechanisticRunner(
+            fake_initial_state,
+            seip_ode,
+            RUNNER_CONFIG_PATH,
+            "random_broken_path",
+        )
+    with pytest.raises(AssertionError):
+        MechanisticRunner(
+            fake_initial_state,
+            seip_ode,
+            "random_broken_path",
+            "random_broken_path2",
+        )
+
+
+def test_external_i_shape():
+    external_i_shape = runner.external_i(100).shape
+    expected_shape = runner.INITIAL_STATE[runner.COMPARTMENT_IDX.I].shape
+    assert (
+        external_i_shape == expected_shape
+    ), "external infections shape incompatible with I compartment"
+
+
+def test_output_shapes():
+    """tests that the ode-model outputs the correct compartment shapes according to the config file it was run in."""
+    state = runner.INITIAL_STATE
+    first_derivatives = ODES(state, 0, runner.get_args(sample=False))
+    expected_output_shapes = [
+        (
+            runner.NUM_AGE_GROUPS,
+            2**runner.NUM_STRAINS,
+            runner.MAX_VAX_COUNT + 1,
+            runner.NUM_WANING_COMPARTMENTS,
+        ),
+        (
+            runner.NUM_AGE_GROUPS,
+            2**runner.NUM_STRAINS,
+            runner.MAX_VAX_COUNT + 1,
+            runner.NUM_STRAINS,
+        ),
+        (
+            runner.NUM_AGE_GROUPS,
+            2**runner.NUM_STRAINS,
+            runner.MAX_VAX_COUNT + 1,
+            runner.NUM_STRAINS,
+        ),
+        (
+            runner.NUM_AGE_GROUPS,
+            2**runner.NUM_STRAINS,
+            runner.MAX_VAX_COUNT + 1,
+            runner.NUM_STRAINS,
+        ),
+    ]
+    for compartment, expected_shape in zip(
+        first_derivatives, expected_output_shapes
+    ):
+        assert compartment.shape == expected_shape, (
+            "at least one compartment output shape does not match expected, was: "
+            + str(compartment.shape)
+            + " expected: "
+            + str(expected_shape)
+        )
