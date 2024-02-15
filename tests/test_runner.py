cb8551963179d0740f808750c349a526ddb3c70f
Ariel Shurygin <uva5@cdc.gov>
2024-02-15 11:33:50 -0800

    realized the tests failed because runner does less now

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -3,6 +3,7 @@ import pytest
 
 from config.config import Config
 from mechanistic_model.mechanistic_runner import MechanisticRunner
+from mechanistic_model.static_value_parameters import StaticValueParameters
 from model_odes.seip_model import seip_ode
 
 CONFIG_GLOBAL_PATH = "tests/test_config_global.json"
@@ -26,39 +27,41 @@ fake_initial_state = (
     1 * jnp.zeros(EIC_SHAPE),  # I
     jnp.zeros(EIC_SHAPE),  # C
 )
-runner = MechanisticRunner(
-    fake_initial_state, seip_ode, RUNNER_CONFIG_PATH, CONFIG_GLOBAL_PATH
+static_params = StaticValueParameters(
+    fake_initial_state,
+    RUNNER_CONFIG_PATH,
+    CONFIG_GLOBAL_PATH,
 )
+runner = MechanisticRunner(seip_ode)
 ODES = seip_ode
 
 
 def test_invalid_paths_raise():
     with pytest.raises(FileNotFoundError):
-        MechanisticRunner(
+        StaticValueParameters(
             fake_initial_state,
-            seip_ode,
             "random_broken_path",
             CONFIG_GLOBAL_PATH,
         ),
     with pytest.raises(FileNotFoundError):
-        MechanisticRunner(
+        StaticValueParameters(
             fake_initial_state,
-            seip_ode,
             RUNNER_CONFIG_PATH,
             "random_broken_path",
         )
     with pytest.raises(FileNotFoundError):
-        MechanisticRunner(
+        StaticValueParameters(
             fake_initial_state,
-            seip_ode,
             "random_broken_path",
             "random_broken_path2",
         )
 
 
 def test_external_i_shape():
-    external_i_shape = runner.external_i(100).shape
-    expected_shape = runner.INITIAL_STATE[runner.COMPARTMENT_IDX.I].shape
+    external_i_shape = static_params.external_i(100).shape
+    expected_shape = static_params.INITIAL_STATE[
+        static_params.COMPARTMENT_IDX.I
+    ].shape
     assert (
         external_i_shape == expected_shape
     ), "external infections shape incompatible with I compartment"
@@ -66,32 +69,33 @@ def test_external_i_shape():
 
 def test_output_shapes():
     """tests that the ode-model outputs the correct compartment shapes according to the config file it was run in."""
-    state = runner.INITIAL_STATE
-    first_derivatives = ODES(state, 0, runner.get_args(sample=False))
+    first_derivatives = ODES(
+        static_params.INITIAL_STATE, 0, static_params.get_parameters()
+    )
     expected_output_shapes = [
         (
-            runner.NUM_AGE_GROUPS,
-            2**runner.NUM_STRAINS,
-            runner.MAX_VAX_COUNT + 1,
-            runner.NUM_WANING_COMPARTMENTS,
+            static_params.NUM_AGE_GROUPS,
+            2**static_params.NUM_STRAINS,
+            static_params.MAX_VAX_COUNT + 1,
+            static_params.NUM_WANING_COMPARTMENTS,
         ),
         (
-            runner.NUM_AGE_GROUPS,
-            2**runner.NUM_STRAINS,
-            runner.MAX_VAX_COUNT + 1,
-            runner.NUM_STRAINS,
+            static_params.NUM_AGE_GROUPS,
+            2**static_params.NUM_STRAINS,
+            static_params.MAX_VAX_COUNT + 1,
+            static_params.NUM_STRAINS,
         ),
         (
-            runner.NUM_AGE_GROUPS,
-            2**runner.NUM_STRAINS,
-            runner.MAX_VAX_COUNT + 1,
-            runner.NUM_STRAINS,
+            static_params.NUM_AGE_GROUPS,
+            2**static_params.NUM_STRAINS,
+            static_params.MAX_VAX_COUNT + 1,
+            static_params.NUM_STRAINS,
         ),
         (
-            runner.NUM_AGE_GROUPS,
-            2**runner.NUM_STRAINS,
-            runner.MAX_VAX_COUNT + 1,
-            runner.NUM_STRAINS,
+            static_params.NUM_AGE_GROUPS,
+            2**static_params.NUM_STRAINS,
+            static_params.MAX_VAX_COUNT + 1,
+            static_params.NUM_STRAINS,
         ),
     ]
     for compartment, expected_shape in zip(
