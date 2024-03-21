9df1d4d5dcaebbc3342137e9112de3d015e0d3c0
Ariel Shurygin <39861882+arik-shurygin@users.noreply.github.com>
2024-03-21 14:56:52 -0700

    checkpoint almost working

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
 FROM python:3.10
 
 COPY . /app
+#input container mount
+RUN mkdir /input
+# ADD /input /input
+# output container mount
+RUN mkdir /output
+# ADD /output /output
+# change cwd to /app
 WORKDIR /app
 RUN pip install -r requirements.txt
-
-CMD ["python", "example_end_to_end_run.py"]
--- azure_job_launcher.py
+++ azure_job_launcher.py
@@ -1,39 +1,54 @@
 from cfa_azure.clients import AzureClient
+import cfa_azure.helpers as helpers
+import os
 
 client = AzureClient(config_path="secrets/configuration_cfaazurebatchprd.toml")
 # client = AzureClient(config_path=None)
 # turn debugging on, this is required
-client.set_debugging(True)
+# client.set_debugging(True)
 
 client.package_and_upload_dockerfile(
-    registry_name="test_registry_scenarios_mechanistic", repo_name="repo1", tag="test"
+    registry_name="cfaprdbatchcr", repo_name="scenarios-test", tag="test2"
 )
 
-# create the input and output containers
-client.create_input_container("example-input-scenarios-mechanistic", "input")
-client.create_output_container("example-output-scenarios-mechanistic", "output")
+# create the input and output blobs
+client.set_input_container("scenarios-test-container", "input")
+client.set_output_container("example-output-scenarios-mechanistic", "output")
 
 # set the scaling of the pool:autoscale
 client.set_scaling(
-    mode="autoscale", autoscale_formula_path="./autoscale_formula.txt"
+    mode="fixed", dedicated_nodes=2#, autoscale_formula_path="./autoscale_formula.txt"
 )
 # if fixed mode is desired, do the following:
 # client.set_scaling(mode="fixed")
 
 # create the pool
-client.create_pool(pool_name="test")
+# client.create_pool(pool_name="scenarios_2_node")
 # or use a certain pool
-client.use_pool(pool_name="test")
+client.use_pool(pool_name="scenarios_2_node")
 
 # upload files
-client.upload_files_in_folder(["data"])
-
-# commad to run the job
-client.add_job(job_id="run_test")
-docker_cmd = "python ./example_end_to_end_run.py"
-client.add_task(job_id="run_test", docker_cmd=docker_cmd)
-client.monitor_job(job_id="run_test")
+# client.upload_files_in_folder(["data"])
+# in_files = []
+# for folder, _, file in os.walk(os.path.realpath("./data")):
+#         for file_name in file:
+#             in_files.append(file_name)
+print("MYPRINTS")
+print(client.input_container_name)
+print(client.config)
+print(client.full_container_name)
+print(client.container_image_name)
+in_files = helpers.list_files_in_container(
+                client.input_container_name, client.sp_credential, client.config
+            ) 
+# command to run the job
+client.add_job(job_id="scenarios_test_run_15", input_files=in_files)
+# docker_cmd = "python ./example_end_to_end_run.py -state "
+# for state in states:
+#     docker_cmd = "python ./example_end_to_end_run.py -state " + str(state)
+client.add_task(job_id="scenarios_test_run_15", docker_cmd="python /app/example_end_to_end_run.py")
+client.monitor_job(job_id="scenarios_test_run_15")
 
 # close down the jobs, required when using debug is True
 # look into https://learn.microsoft.com/en-us/python/api/azure-batch/azure.batch.operations.joboperations?view=azure-python#azure-batch-operations-joboperations terminate
-client.delete_job(job_id="run_test")
+# client.delete_job(job_id="runtest6")
