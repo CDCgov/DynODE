19a104e71288051870ba567c5bf1cba93d8bc9b9
Ariel Shurygin <uva5@cdc.gov>
2024-03-25 15:57:32 -0700

    adding easy checkpointing of mcmc posteriors

--- mechanistic_model/mechanistic_inferer.py
+++ mechanistic_model/mechanistic_inferer.py
@@ -1,6 +1,5 @@
 import copy
 import json
-import os
 import warnings
 
 import jax.numpy as jnp
@@ -37,6 +36,7 @@ class MechanisticInferer(AbstractParameters):
         self.config = Config(global_json).add_file(distributions_json)
         self.runner = runner
         self.INITIAL_STATE = initial_state
+        self.infer_complete = False  # flag once inference completes
         self.set_infer_algo(prior_inferer=prior_inferer)
         self.retrieve_population_counts()
         self.load_cross_immunity_matrix()
@@ -107,7 +107,19 @@ class MechanisticInferer(AbstractParameters):
         )
         # axis = 0 because we take diff across time
         model_incidence = jnp.diff(model_incidence, axis=0)
-
+        # save the final timestep of solution array for each compartment
+        numpyro.deterministic(
+            "final_timestep_s", solution.ys[self.config.COMPARTMENT_IDX.S][-1]
+        )
+        numpyro.deterministic(
+            "final_timestep_e", solution.ys[self.config.COMPARTMENT_IDX.E][-1]
+        )
+        numpyro.deterministic(
+            "final_timestep_i", solution.ys[self.config.COMPARTMENT_IDX.I][-1]
+        )
+        numpyro.deterministic(
+            "final_timestep_c", solution.ys[self.config.COMPARTMENT_IDX.C][-1]
+        )
         # sample infection hospitalization rate here
         with numpyro.plate("num_age", self.config.NUM_AGE_GROUPS):
             ihr = numpyro.sample("ihr", Dist.Beta(0.5, 10))
@@ -334,86 +346,36 @@ class MechanisticInferer(AbstractParameters):
             obs_metrics=obs_metrics,
         )
         self.inference_algo.print_summary()
+        self.infer_complete = True
         return self.inference_algo
 
-    def checkpoint(self, checkpoint_folder):
+    def checkpoint(self, checkpoint_path):
         """
-        a function which saves the posterior samples from `self.inference_algo.get_samples()` into `checkpoint_folder` as well
-        as the average final state of the model when using all particles found in `self.inference_algo.get_samples()`.
+        a function which saves the posterior samples from `self.inference_algo` into `checkpoint_path` as a json file.
+        will save anything sampled or numpyro.deterministic as long as it is tracked by `self.inference_algo`
+
+        Parameters
+        -----------
+        checkpoint_path: str
+            a path to which the json file is saved to. Throws error if folders do not exist, overwrites existing JSON files within.
+
+        Returns
+        -----------
+        None
         """
-        if self.inference_algo.last_state is None:
+        if not self.infer_complete:
             print(
                 "unable to checkpoint as you have not called infer() yet to produce posteriors"
             )
             return
-        # get posterior samples
-        samples = self.inference_algo.get_samples(group_by_chain=False)
+        # get posterior samples including any calls to numpyro.deterministic
+        samples = self.inference_algo._states_flat[
+            self.inference_algo._sample_field
+        ]
         # we cant convert ndarray to samples, so we convert to list first
         for parameter in samples.keys():
             param_samples = samples[parameter]
             if isinstance(param_samples, (np.ndarray, jnp.ndarray)):
                 samples[parameter] = param_samples.tolist()
-        with open(os.path.join(checkpoint_folder, "sample.json"), "w") as file:
+        with open(checkpoint_path, "w") as file:
             json.dump(samples, file)
-
-    def create_average_final_state(self, tf):
-        if self.inference_algo.last_state is None:
-            print(
-                "unable to checkpoint as you have not called infer() yet to produce posteriors"
-            )
-            return
-        samples = self.inference_algo.get_samples(group_by_chain=True)
-        # lets flatten any parameters that come as lists into separate parameters
-        samples = utils.flatten_list_parameters(samples)
-        parameters_sampled = samples.keys()
-        # now lets resize it to get rid of the chains dimension
-        # current shape is (# params, num chains, num samples per chain)
-        samples = np.array(samples.values())
-        particles = samples.reshape(
-            (samples.shape[0], samples.shape[1] * samples.shape[2])
-        )  # (num_parameters, num_samples)
-        num_particles = particles.shape[1]
-        config_with_distributions = copy.deepcopy(self.config)
-        all_final_states = [
-            [],
-            [],
-            [],
-            [],
-        ]
-        # for each particle, change self.config parameters to match the posterior parameter values
-        # where applicable, sometimes we sample things like IHR which are not used by the runner
-        for particle in range(num_particles):
-            for i, parameter in enumerate(parameters_sampled):
-                posterior_param_value = samples[i, particle]
-                # if parameter name matches what is in self.config
-                if hasattr(self.config, parameter):
-                    setattr(self.config, parameter, posterior_param_value)
-                # if we sample as a part of a list, the end of `parameter` will have _i where i is the index
-                elif hasattr(self.config, "".join(parameter.split("_")[:-1])):
-                    # remove the _i from the name so we can reference it in self.config
-                    param_name_no_index = "".join(parameter.split("_")[:-1])
-                    # grab the index so we can set that element of the list
-                    param_index = int(parameter[-1])
-                    self.config.__dict__[param_name_no_index][
-                        param_index
-                    ] = posterior_param_value
-            # with self.config changed out, there are no distributions to sample, so calling self.get_parameters()
-            # returns only values and no numpyro.sample tracers
-            run_with_posterior_params = self.runner.run(
-                self.INITIAL_STATE,
-                self.get_parameters(),
-                tf=tf,
-            )
-            for i, compartment in enumerate(run_with_posterior_params.ys):
-                # timestep is first dimension, so we selecting compartment at t=-1
-                # which in this case is t=240
-                last_time_step = compartment[-1]
-                # add a rolling sum to the average_final_state variable
-                all_final_states[i].append(last_time_step)
-                # total_pops[i] += np.sum(last_time_step)
-        average_final_state = [
-            np.mean(compartment, axis=0) for compartment in all_final_states
-        ]
-        # set our self.config back to what it was in case we use inferer again
-        self.config = config_with_distributions
-        return average_final_state
