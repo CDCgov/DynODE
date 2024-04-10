5f9f9e11f0aec7cc3dd338c91c48e918b08d3c4a
Ben Toh <tjk3@cdc.gov>
2024-04-10 16:40:49 +0000

    Add/modified R scripts for sero and immunity
    - Modified immunity waning script in line with faster waning
    - Seroprevalence adjustment script converting Garcia-Carreras 2023 to
      all age seroprevalence
    - Modified seroprevalence script for blood donor: now adjust for all
      states based on adjusted seroprevalence
    - Seroprevalence script to essentially stack blood donor with pediatric
      seroprevalence

---
+++ data_manipulation_scripts/seroprevalence_combined.R
@@ -0,0 +1,77 @@
+####################################################
+# Combine all available data for seroprevalence from
+# 2022-02-11 onwards
+####################################################
+
+pacman::p_load(dplyr, ggplot2, lubridate, tidyr, glue)
+theme_set(theme_bw(base_size = 13))
+theme_update(
+  text = element_text(family = "Open Sans"),
+  strip.background = element_blank(),
+  strip.text = element_text(face = "bold"),
+  panel.grid = element_line(linewidth = rel(0.43), colour = "#D1D3D4"),
+  panel.border = element_rect(linewidth = rel(0.43)),
+  axis.ticks = element_line(linewidth = rel(0.43))
+)
+
+# Blood donor seroprevalence
+donor_csv <- file.path(
+  "data",
+  "serological-data",
+  "donor2022.csv"
+)
+donor_df <- data.table::fread(donor_csv) |>
+  select(-quarter) |>
+  mutate(date = as.Date(date))
+
+# Commercial lab pediatric seroprevalence
+lab_csv <- file.path(
+  "data",
+  "serological-data",
+  "Nationwide_Commercial_Laboratory_Seroprevalence_Survey_20231018.csv"
+)
+lab_df <- data.table::fread(lab_csv)
+
+## Extract only pediatric data from Round 31 onwards
+## Represent data using middle of date range
+cols <- c(
+  "n [Anti-N, 0-17 Years Prevalence]",
+  "Rate (%) [Anti-N, 0-17 Years Prevalence]"
+)
+
+lab_pediatric <- lab_df |>
+  filter(`Date Range of Specimen Collection` != "", Round > 30) |>
+  select(Site, `Date Range of Specimen Collection`, all_of(cols)) |>
+  separate_wider_delim(
+    `Date Range of Specimen Collection`,
+    delim = "-", names = c("start_date", "end_date")
+  ) |>
+  mutate(
+    end_date = mdy(end_date),
+    start_date = ifelse(stringr::str_detect(start_date, ","),
+      start_date, glue("{start_date}{year(end_date)}")
+    ),
+    start_date = mdy(start_date),
+    mid_date = start_date + (end_date - start_date) / 2
+  )
+
+lab_pediatric <- lab_pediatric |>
+  select(
+    region = Site, date = mid_date,
+    n = `n [Anti-N, 0-17 Years Prevalence]`,
+    rate = `Rate (%) [Anti-N, 0-17 Years Prevalence]`
+  ) |>
+  filter(rate < 100) |>
+  mutate(age = "0-17")
+
+# Stack lab and donor
+comb_df <- bind_rows(donor_df, lab_pediatric)
+with(comb_df, table(region, age))
+
+comb_df |>
+  filter(region != "US") |>
+  ggplot() +
+  geom_point(aes(x = date, y = rate, colour = age, size = n)) +
+  facet_wrap(~region)
+
+data.table::fwrite(comb_df, "./data/serological-data/combined2022.csv")
