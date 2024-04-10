df3e1ddad801149821d3f325810237576659faeb
Ariel Shurygin <39861882+arik-shurygin@users.noreply.github.com>
2024-04-10 13:02:00 -0700

    freezing the cfa_azure library to a particular commit

--- experiment_launcher_azure.py
+++ experiment_launcher_azure.py
@@ -10,7 +10,7 @@ import cfa_azure.helpers as helpers
 from cfa_azure.clients import AzureClient
 
 # specify job ID, cant already exist
-JOB_ID = "scenarios_inference_run_12"
+JOB_ID = "scenarios_inference_run_14"
 # number of seconds of a full experiment run before timeout
 # for `s` states to run and `n` nodes dedicated,`s/n` * runtime 1 state secs needed
 TIMEOUT_MINS = 120
@@ -36,24 +36,19 @@ client = AzureClient(config_path="secrets/configuration_cfaazurebatchprd.toml")
 client.package_and_upload_dockerfile(
     registry_name="cfaprdbatchcr", repo_name="scenarios", tag="expinfer3"
 )
-
 # create the input and output blobs, for now they must be named /input and /output
 client.set_input_container("scenarios-test-container", "input")
 client.set_output_container("example-output-scenarios-mechanistic", "output")
 
 # set the scaling of the pool, assign `dedicated_nodes` to split work accross
-client.set_scaling(mode="fixed", dedicated_nodes=2, timeout=TIMEOUT_MINS)
+client.set_scaling(mode="fixed", dedicated_nodes=5, timeout=TIMEOUT_MINS)
 # create the pool
-# client.create_pool(pool_name="scenarios_2_node")
+client.create_pool(pool_name="scenarios_5_node")
 # or use a certain pool if already exists and active
-client.use_pool(pool_name="scenarios_2_node")
+# client.use_pool(pool_name="scenarios_2_node")
 
-# for now lets just mount every file into each node
-in_files = helpers.list_files_in_container(
-    client.input_container_name, client.sp_credential, client.config
-)
 # command to run the job
-client.add_job(job_id=JOB_ID, input_files=in_files)
+client.add_job(job_id=JOB_ID)
 # add a task for each subdir of the given experiment folder
 for subdir in os.listdir(args.folder):
     subdir_path = os.path.join(args.folder, subdir)
@@ -63,7 +58,6 @@ for subdir in os.listdir(args.folder):
         # since experiment directories are structured with USPS state codes as directory names
         client.add_task(
             job_id=JOB_ID,
-            task_id=JOB_ID + subdir,
             docker_cmd="python %s -s %s" % (args.runner, subdir),
         )
 client.monitor_job(job_id=JOB_ID)
