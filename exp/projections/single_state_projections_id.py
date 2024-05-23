f9eab7a346b21d3d6a70d02407fe46f567cd302c
Ben Toh <tjk3@cdc.gov>
2024-05-23 19:16:46 +0000

    Minor fix of importing parameters

--- exp/projections/single_state_projections_id.py
+++ exp/projections/single_state_projections_id.py
@@ -16,11 +16,12 @@ sys.path.append("/app")
 sys.path.append("/app/mechanistic_model/")
 
 import utils
-from exp.fifty_state_2304_2404_3strain.epoch_two_initializer import \
-    smh_initializer_epoch_two
+from exp.fifty_state_2304_2404_3strain.epoch_two_initializer import (
+    smh_initializer_epoch_two,
+)
 from exp.fifty_state_2304_2404_3strain.inferer_smh import SMHInferer
 from mechanistic_model.mechanistic_runner import MechanisticRunner
-from model_odes.seip_model import Parameters, seip_ode
+from model_odes.seip_model import seip_ode
 
 """
  
