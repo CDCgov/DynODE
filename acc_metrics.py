58ca81bb77e55ded12100d2beef5ef1b50d5857c
Ariel Shurygin <arik.shurygin@gmail.com>
2024-07-24 21:57:39 +0000

    improved runtime

--- acc_metrics.py
+++ acc_metrics.py
@@ -5,7 +5,8 @@ from exp.fifty_state_5strain_2202_2404.postaz_process import (
     simulate_hospitalization,
 )
 import arviz as az
-from sklearn.metrics import mean_squared_error
+
+# from sklearn.metrics import mean_squared_error
 import numpy as np
 import jax.numpy as jnp
 import jax
@@ -19,9 +20,12 @@ import numpyro.distributions as dist
 # from numpyro import log_prob
 import os
 import numpyro
-import multiprocessing as mp
 
-# from exp.fifty_state_5strain_2202_2404.inferer_smh import SMHInferer
+# import multiprocessing as mp
+
+from exp.fifty_state_5strain_2202_2404.inferer_smh import SMHInferer
+
+jax.config.update("jax_enable_x64", True)
 
 
 def hosp_var_posterior(
@@ -35,7 +39,9 @@ def hosp_var_posterior(
         list(range(nsamp)), particles_per_chain
     )  # Randomly pick some samples per chain
     all_samples = [
-        {k: v[c][r] for k, v in samp.items()} for r in ranindex for c in range(nchain)
+        {k: v[c][r] for k, v in samp.items()}
+        for r in ranindex
+        for c in range(nchain)
     ]
     pred_hosps_list = []
     pred_var_list = []
@@ -79,7 +85,7 @@ def hosp_var_posterior(
 
 
 def mcmc_accuracy_measures(state, particles_per_chain):
-
+    print("getting samples via json.load()")
     samp, fitted_means = retrieve_post_samp(state)
     (
         inferer,
@@ -90,36 +96,88 @@ def mcmc_accuracy_measures(state, particles_per_chain):
         obs_sero_days,
         obs_var_prop,
         obs_var_days,
-    ) = retrieve_inferer_obs(
-        state
-    )  # , initial_model_day=0)
-    pred_hosps_list, pred_var_list, ranindex = hosp_var_posterior(
-        samp, inferer, runner, particles_per_chain
+    ) = retrieve_inferer_obs(state)
+    print("got inferer")
+    # get random particle_index
+    particle_indexes = np.random.choice(
+        range(inferer.config.INFERENCE_NUM_SAMPLES),
+        particles_per_chain,
+        replace=False,
     )
+    # select the particle for each chain make a list of tuples
+    chain_particle_pairs = [
+        (chain, particle)
+        for chain in range(inferer.config.INFERENCE_NUM_CHAINS)
+        for particle in particle_indexes
+    ]
+    # create your big list of hosp and var timeseries
+    pred_hosps_list = []
+    pred_vars_list = []
+    # get a solutiong dict of (chain, particle) keys leading to the solution objects and hosp timelines
+    posteriors_solution_dct = inferer.load_posterior_particle(
+        chain_particle_pairs, max(obs_hosps_days) + 1, samp
+    )
+    nchain = len(samp["ihr_3"])
+    # go through each chain and particle and build up a digestible list of the hosp and var props
+    for chain in range(nchain):
+        pred_hosps_chain = []
+        pred_vars_chain = []
+        for particle_index in particle_indexes:
+            sol_dct = posteriors_solution_dct[(chain, particle_index)]
+            # get the solution object and the predicted hosp out of the solution dct
+
+            output = sol_dct["solution"]
+            hosps = sol_dct["hospitalizations"]
+            strain_incidence = jnp.sum(
+                output.ys[inferer.config.COMPARTMENT_IDX.C],
+                axis=(
+                    inferer.config.I_AXIS_IDX.age + 1,
+                    inferer.config.I_AXIS_IDX.hist + 1,
+                    inferer.config.I_AXIS_IDX.vax + 1,
+                ),
+            )
+            strain_incidence = jnp.diff(strain_incidence, axis=0)
+            pred_vars = strain_incidence / jnp.sum(
+                strain_incidence, axis=-1, keepdims=True
+            )
+            # select only obs hosp days
+            pred_hosps_chain.append(
+                jnp.array(hosps)[jnp.array(obs_hosps_days), ...]
+            )
+            pred_vars_chain.append(pred_vars)
+        pred_hosps_list.append(pred_hosps_chain)
+        pred_vars_list.append(pred_vars_chain)
+    # pred_hosps_list, pred_var_list, ranindex = hosp_var_posterior(
+    #     samp, inferer, runner, particles_per_chain
+    # )
     # pred_hosps_list.shape == (nchain, ranindex, time_series, age_group)
     # pred_var_list.shape == (nchain, ranindex, time_series, strains)
     # obs_hosps.shape == (112, 4)
     nchain = len(samp["ihr_3"])
-    obs_hosps = jnp.tile(jnp.array(obs_hosps), (nchain, len(ranindex), 1, 1))
     log_likelihood_array = []
+    # go through each chain/sample and get the log likelihood of pred hosp given obs hosp
     print("starting log-likelihood setup")
     for pred_hosps_chain in pred_hosps_list:
         log_likelihood_chain = []
         for pred_hosps in pred_hosps_chain:
             # Ensure that pred_hosps and obs_hosps_interval have the same shape
-            pred_hosps = jnp.array(pred_hosps)
-            pred_hosps = pred_hosps[jnp.array(obs_hosps_days), :]
             mask_incidence = ~jnp.isnan(obs_hosps)
             with numpyro.handlers.mask(mask=mask_incidence):
                 log_likelihood = dist.Poisson(pred_hosps).log_prob(obs_hosps)
             log_likelihood_chain.append(log_likelihood)
         log_likelihood_array.append(log_likelihood_chain)
-    print("finishing it")
+
+    # repeat obs_hosp chain * sample times so the shape matches the pred hosp arrays
+    obs_hosps = jnp.tile(
+        jnp.array(obs_hosps), (nchain, len(particle_indexes), 1, 1)
+    )
     # log_likelihood_array.shape == (nchains, ranindex, 112, 4)
     # pred_hosps_list.shape == (nchains, ranindex, 112, 4)
-    # Randomly pick some samples per chain
+    # get the posterior values for each chain to pass to the posteriors
+    # TODO, possible bug here if posteriors_selected has different ordering than pred_hosps_list
     posteriors_selected = {
-        key: np.array(value)[:, ranindex] for key, value in samp.items()
+        key: np.array(value)[:, particle_indexes]
+        for key, value in samp.items()
     }
 
     trace_hosps = az.from_dict(
@@ -130,6 +188,7 @@ def mcmc_accuracy_measures(state, particles_per_chain):
     )
 
     waic_hosps = az.waic(trace_hosps)
+    print(waic_hosps)
     rmse = {}
     # rmse_hosp = np.sqrt(
     #     mean_squared_error(
@@ -242,4 +301,4 @@ print(states)
 # )
 # pool.close()
 # pd.concat(df_total).to_csv(f"output/accuracy.csv", index=False)
-mcmc_accuracy_measures(state="MN", particles_per_chain=2)
+mcmc_accuracy_measures(state="AK", particles_per_chain=50)
