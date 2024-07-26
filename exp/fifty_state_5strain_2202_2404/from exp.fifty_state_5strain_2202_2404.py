f88913a5fcce8c234f9264e9794067fbedc217cf
Antonio Gondim <znm9@cdc.gov>
2024-07-26 03:00:32 +0000

    old acc_metric file added and changes on the new version

--- exp/fifty_state_5strain_2202_2404/from exp.fifty_state_5strain_2202_2404.py
+++ exp/fifty_state_5strain_2202_2404/from exp.fifty_state_5strain_2202_2404.py
@@ -1,32 +1,33 @@
-from exp.fifty_state_6strain_2202_2407.postaz_process import (
+from exp.fifty_state_5strain_2202_2404.postaz_process import (
     retrieve_inferer_obs,
     retrieve_post_samp,
+    replace_and_simulate,
+    simulate_hospitalization,
 )
 import arviz as az
 
-from sklearn.metrics import mean_squared_error
+# from sklearn.metrics import mean_squared_error
 import numpy as np
 import jax.numpy as jnp
 import jax
+from jax import vmap, jit
+from functools import partial
 import random
 import numpy as np
 import pandas as pd
 import numpyro.distributions as dist
+
+# from numpyro import log_prob
 import os
 import numpyro
-
-# from mechanistic_model import mechanistic_inferer
-# from mechanistic_model.mechanistic_inferer import load_posterior_particle
-
 import multiprocessing as mp
-from exp.fifty_state_6strain_2202_2407.inferer_smh import SMHInferer
+
+from exp.fifty_state_5strain_2202_2404.inferer_smh import SMHInferer
 
 jax.config.update("jax_enable_x64", True)
 
 
-def mcmc_accuracy_measures(
-    state, particles_per_chain, initial_model_day, variant=False
-):
+def mcmc_accuracy_measures(state, particles_per_chain):
     print("getting samples via json.load()")
     samp, fitted_means = retrieve_post_samp(state)
     (
@@ -38,7 +39,7 @@ def mcmc_accuracy_measures(
         obs_sero_days,
         obs_var_prop,
         obs_var_days,
-    ) = retrieve_inferer_obs(state, initial_model_day)
+    ) = retrieve_inferer_obs(state)
     print("got inferer")
     # get random particle_index
     particle_indexes = np.random.choice(
@@ -84,33 +85,28 @@ def mcmc_accuracy_measures(
             )
             # select only obs hosp days
             pred_hosps_chain.append(jnp.array(hosps)[jnp.array(obs_hosps_days), ...])
-            pred_vars_chain.append(jnp.array(pred_vars)[jnp.array(obs_var_days), ...])
+            pred_vars_chain.append(pred_vars)
         pred_hosps_list.append(pred_hosps_chain)
         pred_vars_list.append(pred_vars_chain)
-
     # pred_hosps_list.shape == (nchain, ranindex, time_series, age_group)
     # pred_var_list.shape == (nchain, ranindex, time_series, strains)
     # obs_hosps.shape == (112, 4)
     nchain = len(samp["ihr_3"])
-    log_likelihood_array_hosps = []
+    log_likelihood_array = []
     # go through each chain/sample and get the log likelihood of pred hosp given obs hosp
     print("starting log-likelihood setup")
     for pred_hosps_chain in pred_hosps_list:
-        log_likelihood_chain_hosps = []
+        log_likelihood_chain = []
         for pred_hosps in pred_hosps_chain:
             # Ensure that pred_hosps and obs_hosps_interval have the same shape
             mask_incidence = ~jnp.isnan(obs_hosps)
             with numpyro.handlers.mask(mask=mask_incidence):
                 log_likelihood = dist.Poisson(pred_hosps).log_prob(obs_hosps)
-            log_likelihood_chain_hosps.append(log_likelihood)
-        log_likelihood_array_hosps.append(log_likelihood_chain_hosps)
+            log_likelihood_chain.append(log_likelihood)
+        log_likelihood_array.append(log_likelihood_chain)
 
-    # repeat obs_hosp, obs_var_prop chain * sample times so the shape matches the pred hosp arrays
+    # repeat obs_hosp chain * sample times so the shape matches the pred hosp arrays
     obs_hosps = jnp.tile(jnp.array(obs_hosps), (nchain, len(particle_indexes), 1, 1))
-    obs_var_prop = jnp.tile(
-        jnp.array(obs_var_prop), (nchain, len(particle_indexes), 1, 1)
-    )
-
     # log_likelihood_array.shape == (nchains, ranindex, 112, 4)
     # pred_hosps_list.shape == (nchains, ranindex, 112, 4)
     # get the posterior values for each chain to pass to the posteriors
@@ -122,53 +118,49 @@ def mcmc_accuracy_measures(
     trace_hosps = az.from_dict(
         posterior_predictive={"hospitalizations": pred_hosps_list},
         observed_data={"hospitalizations": obs_hosps},
-        log_likelihood={"log likelihood:": log_likelihood_array_hosps},
+        log_likelihood={"log likelihood:": log_likelihood_array},
         posteriors=posteriors_selected,
     )
 
     waic_hosps = az.waic(trace_hosps)
-    df_waic_hosps = pd.DataFrame(waic_hosps)
     print(waic_hosps)
-    df_waic_vars = pd.DataFrame()
+    rmse = {}
+    # rmse_hosp = np.sqrt(
+    #     mean_squared_error(
+    #         jnp.median(jnp.array(pred_hosps_list), axis=0,1)[
+    #             jnp.array(obs_hosps_days), :
+    #         ],
+    #         jnp.array(obs_hosps),
+    #     )
+    # )
+    # for strain in range(jnp.array(pred_var_list)[0].shape[-1]):
+    #     rmse[f"RMSE strain {strain}"] = np.sqrt(
+    #         mean_squared_error(
+    #             jnp.median(
+    #                 jnp.array(pred_var_list[:, jnp.array(obs_hosps_days), strain]),
+    #                 axis=(0, 1),
+    #             ),
+    #             obs_hosps,
+    #         )
+    #     )
+    df_rmse = pd.DataFrame(rmse)
+    # df_rmse["RMSE Hospitalizations"] = rmse_hosp
 
-    if variant == True:
-        log_likelihood_array_vars = []
-        for pred_vars_chain in pred_vars_list:
-            log_likelihood_chain_vars = []
-            for pred_vars in pred_vars_chain:
-                mask_incidence = ~jnp.isnan(obs_hosps)
-                with numpyro.handlers.mask(mask=mask_incidence):
-                    pred_vars_sd = jnp.ones(jnp.shape(jnp.array(pred_vars))) * jnp.std(
-                        jnp.array(obs_var_prop)
-                    )
-                    log_likelihood = dist.Normal(pred_vars, pred_vars_sd).log_prob(
-                        obs_var_prop
-                    )
-                log_likelihood_chain_vars.append(log_likelihood)
-            log_likelihood_array_vars.append(log_likelihood_chain_hosps)
+    df_waic = pd.DataFrame(waic_hosps)
+    # df_waic["WAIC for hospitalizations"] = waic_hosps
+    df_total = pd.concat([df_waic, df_rmse])
 
-        trace_hosps = az.from_dict(
-            posterior_predictive={"hospitalizations": pred_vars_list},
-            observed_data={"hospitalizations": obs_var_prop},
-            log_likelihood={"log likelihood:": log_likelihood_array_vars},
-            posteriors=posteriors_selected,
-        )
-        waic_vars = az.waic(trace_hosps)
-        df_waic_vars = pd.DataFrame(waic_vars)
-
-    df_waic = pd.concat([df_waic_hosps, df_waic_vars], axis=1)
-    print(df_waic)
-    return df_waic.to_csv("")
+    return df_total
 
 
 states = [
     "AL",
-    "AK",
+    # "AK",
     # "AZ",
     # "AR",
-    "CA",
-    "CO",
-    "CT",
+    # "CA",
+    # "CO",
+    # "CT",
     # "DE",
     # # "FL",
     # # "GA",
@@ -184,34 +176,34 @@ states = [
     # # "MD",
     # # "MA",
     # # "MI",
-    # "MN",
-    # "MS",
-    # "MO",
-    # "MT",
-    # "NE",
-    # "NV",
-    # "NH",
-    # "NJ",
-    # "NM",
-    # "NY",
-    # "NC",
-    # "ND",
-    # "OH",
-    # "OK",
-    # "OR",
-    # "PA",
-    # "RI",
-    # "SC",
-    # "SD",
-    # "TN",
-    # "TX",
-    # "UT",
-    # "VT",
-    # "VA",
-    # "WA",
-    # "WV",
-    # "WI",
-    # "WY",
+    "MN",
+    # # "MS",
+    # # "MO",
+    # # "MT",
+    # # "NE",
+    # # "NV",
+    # # "NH",
+    # # "NJ",
+    # # "NM",
+    # # "NY",
+    # # "NC",
+    # # "ND",
+    # # "OH",
+    # # "OK",
+    # # "OR",
+    # # "PA",
+    # # "RI",
+    # # "SC",
+    # # "SD",
+    # # "TN",
+    # # "TX",
+    # # "UT",
+    # # "VT",
+    # # "VA",
+    # # "WA",
+    # # "WV",
+    # # "WI",
+    # # "WY",
 ]
 states_omit = [
     "WV",
@@ -223,23 +215,24 @@ states_omit = [
     "VT",
     "VA",
 ]
-az_output_path = "/output/fifty_state_6strain_2204_2407/smh_6str_prelim_6/"
-# for st in states:
-#     json_file = os.path.join(az_output_path, st, "checkpoint.json")
-#     if not os.path.exists(json_file):
-#         states_omit.append(st)
-# states = list(set(states).difference(set(states_omit)))
-# states.sort()
+az_output_path = "/output/fifty_state_5strain_2202_2404/SMH_5strains_071624/"
 
-# print(states)
-# # %%
-# pool = mp.Pool(5)
-# df_total = zip(
-#     *pool.map(
-#         mcmc_accuracy_measures(state=st, particles_per_chain=5),
-#         [st for st in states],
-#     )
-# )
-# pool.close()
-# pd.concat(df_total).to_csv(f"output/accuracy.csv", index=False)
-mcmc_accuracy_measures(state="AK", particles_per_chain=10, initial_model_day=0)
+for st in states:
+    json_file = os.path.join(az_output_path, st, "checkpoint.json")
+    if not os.path.exists(json_file):
+        states_omit.append(st)
+states = list(set(states).difference(set(states_omit)))
+states.sort()
+# states = ["US", "AK", "AL", "IL", "WA"]
+# states = ["US"] + states
+print(states)
+# %%
+pool = mp.Pool(5)
+df_total = zip(
+    *pool.map(
+        mcmc_accuracy_measures(state=st, particles_per_chain=2),
+        [st for st in states],
+    )
+)
+pool.close()
+pd.concat(df_total).to_csv(f"output/accuracy.csv", index=False)
