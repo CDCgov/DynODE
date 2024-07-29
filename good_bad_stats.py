71c0f31242ebc158af537b1d29bee4114644cda7
Antonio Gondim <znm9@cdc.gov>
2024-07-29 16:32:14 +0000

    changes mad ein the good and bad stats for the model comparasion

--- good_bad_stats.py
+++ good_bad_stats.py
@@ -134,14 +134,14 @@ if __name__ == "__main__":
                 s1 = df1.iloc[1, k]
                 s2 = df2.iloc[1, k]
                 elpd1, elpd2, s1, s2 = float(elpd1), float(elpd2), float(s1), float(s2)
-                z_score = (elpd1 - elpd2) / (np.sqrt(s1**2 + s2**2))
+                z_score = np.abs(elpd1 - elpd2) / (np.sqrt(s1**2 + s2**2))
                 if abs(z_score) > 2:
                     l.append(
-                        f"hospitalizations for state {df1.columns[k]} is significant"
+                        f"hospitalizations accuracy difference for state {df1.columns[k]} is significant"
                     )
                 else:
                     l.append(
-                        f"hospitalizations for state {df1.columns[k]} insignificant"
+                        f"hospitalizations accuracy difference for state {df1.columns[k]} insignificant"
                     )
             l = pd.Series(l, index=df1.columns[1:])
             df1 = pd.read_csv(f"output/accuracy{suffix1}.csv")
@@ -153,14 +153,14 @@ if __name__ == "__main__":
                 s1 = df1.iloc[8, k]
                 s2 = df2.iloc[8, k]
                 elpd1, elpd2, s1, s2 = float(elpd1), float(elpd2), float(s1), float(s2)
-                z_score = (elpd1 - elpd2) / (np.sqrt(s1**2 + s2**2))
+                z_score = np.abs(elpd2 - elpd1) / (np.sqrt(s1**2 + s2**2))
                 if abs(z_score) > 2:
                     l1.append(
-                        f"hospitalizations accuracy for state {df2.columns[k]} is significant"
+                        f"variant prop accuracy difference for state {df2.columns[k]} is significant"
                     )
                 else:
                     l1.append(
-                        f"variant_prop accuracy for state {df2.columns[k]} insignificant"
+                        f"variant_prop accuracy difference for state {df2.columns[k]} insignificant"
                     )
             l1 = pd.Series(l, index=df1.columns[1:])
             return l, l1
