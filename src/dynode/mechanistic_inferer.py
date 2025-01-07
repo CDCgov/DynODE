e11e4fd2967aef6ff031ceb7eed6e9a286192b8b
Ariel Shurygin <arik.shurygin@gmail.com>
2025-01-07 20:27:12 +0000

    fixing one lingering mypy error

--- src/dynode/mechanistic_inferer.py
+++ src/dynode/mechanistic_inferer.py
@@ -260,8 +260,8 @@ class MechanisticInferer(AbstractParameters):
                 "final_timestep_%s" % compartment.name,
                 solution.ys[compartment][-1],
             )
-        for date in getattr(self.config, "COMPARTMENT_SAVE_DATES", []):
-            date: datetime.date
+        for d in getattr(self.config, "COMPARTMENT_SAVE_DATES", []):
+            date: datetime.date = d
             date_str = date.strftime("%Y_%m_%d")
             sim_day = date_to_sim_day(date, self.config.INIT_DATE)
             # ensure user requests a day we actually have in `solution`
