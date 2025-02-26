007c97661ee2f1bd70b763bf011a37b005c86b80
Ariel Shurygin <uva5@cdc.gov>
2025-02-25 16:02:52 -0800

    adding PosteriorSample as optional type within DynODE configs

---
+++ src/dynode/model_configuration/types.py
@@ -0,0 +1,33 @@
+"""Module for declaring types to be used within DynODE config files."""
+
+import numpyro.distributions as dist
+
+
+class SamplePosteriorError(Exception):
+    """A special error raised if you attempt to randomly sample a deterministic posterior draw."""
+
+    pass
+
+
+class PosteriorSample(dist.Distribution):
+    """A parameter that draws its values from an external set of posterior samples."""
+
+    def __init__(self):
+        """Create a placeholder PosteriorSample distribution."""
+        super(PosteriorSample, self).__init__()
+
+    def sample(self, _, sample_shape=()):
+        """Retrieve sample from a Posterior distribution.
+
+        Raises
+        ------
+        SamplePosteriorError
+            if sample is called outside of an in-place substitute context like
+            numpyro.handlers.substitute() or numpyro.infer.Predictive.
+        """
+        raise SamplePosteriorError(
+            """Attempted to sample a PosteriorSample parameter outside of a
+            Predictive() context. This likely means you did not provide
+            posterior samples to the context via numpyro.infer.Predictive() or
+            numpyro.handlers.substitute()."""
+        )
