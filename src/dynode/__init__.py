f4ab9cf484825f19937e489ac7e4e1a85a703525
Ariel Shurygin <arik.shurygin@gmail.com>
2025-05-20 22:34:50 +0000

    allowing users to access all dynode objects from top level, experimental change for now

--- src/dynode/__init__.py
+++ src/dynode/__init__.py
@@ -9,6 +9,18 @@ refactored in the near future!
 """
 
 from . import config, infer, simulate, typing, utils
+from .config import __all__ as config_all
+from .infer import __all__ as infer_all
+from .simulate import __all__ as simulate_all
+from .typing import __all__ as typing_all
+from .utils import __all__ as utils_all
 
 # Defines all the different modules able to be imported from src
 __all__ = ["config", "infer", "utils", "simulate", "typing"]
+# Append the __all__ of all submodules to the main __all__
+
+__all__.extend(config_all)
+__all__.extend(infer_all)
+__all__.extend(simulate_all)
+__all__.extend(typing_all)
+__all__.extend(utils_all)
