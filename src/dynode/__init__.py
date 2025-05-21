12158aed914423a55bfbb03e7c36c928152dc48f
Ariel Shurygin <arik.shurygin@gmail.com>
2025-05-21 18:41:12 +0000

    flattening namespace of dynode

--- src/dynode/__init__.py
+++ src/dynode/__init__.py
@@ -8,19 +8,18 @@ DynODE is currently under active development and will be substantially
 refactored in the near future!
 """
 
+import importlib
+
 from . import config, infer, simulate, typing, utils
-from .config import __all__ as config_all
-from .infer import __all__ as infer_all
-from .simulate import __all__ as simulate_all
-from .typing import __all__ as typing_all
-from .utils import __all__ as utils_all
 
 # Defines all the different modules able to be imported from src
 __all__ = ["config", "infer", "utils", "simulate", "typing"]
+submodules = ["config", "infer", "utils", "simulate", "typing"]
 # Append the __all__ of all submodules to the main __all__
-
-__all__.extend(config_all)
-__all__.extend(infer_all)
-__all__.extend(simulate_all)
-__all__.extend(typing_all)
-__all__.extend(utils_all)
+for submodule in submodules:
+    module = importlib.import_module(f".{submodule}", package="dynode")
+    if hasattr(module, "__all__"):
+        for attr in module.__all__:
+            globals()[attr] = getattr(module, attr)
+            __all__.append(attr)
+# effictively flattens all submodules into dynode namespace.
