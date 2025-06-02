b472e30c03c05028f4f45726d5c5b2723f14d791
Ariel Shurygin <arik.shurygin@gmail.com>
2025-06-02 23:16:56 +0000

    finishing basic params tests

--- tests/test_config/test_params.py
+++ tests/test_config/test_params.py
@@ -41,7 +41,7 @@ def test_transmission_params_invalid_strain_interactions():
         config.TransmissionParams(
             strains=strains, strain_interactions=strain_interactions
         )
-    # Extra strain in interactions not in strains, TODO have this raise
+    # Extra strain in interactions not in strains, TODO have this raise PR #420 fixes this
     # strain_interactions = {
     #     "strain1": {"strain1": 1.0, "strain2": 0.5, "strain3": 0.5},
     #     "strain2": {"strain1": 0.5, "strain2": 1.0, "strain3": 0.5},
@@ -51,3 +51,73 @@ def test_transmission_params_invalid_strain_interactions():
     #     config.TransmissionParams(
     #         strains=strains, strain_interactions=strain_interactions
     #     )
+
+
+def test_transmission_params_inconsistent_strains():
+    """Test that the TransmissionParams raises an error for inconsistent strains."""
+    strains = [
+        config.Strain(
+            strain_name="strain1",
+            r0=2,
+            infectious_period=5,
+            exposed_to_infectious=5.0,
+        ),
+        config.Strain(
+            strain_name="strain2", r0=2, infectious_period=5
+        ),  # missing exposed_to_infectious
+    ]
+    strain_interactions = {
+        "strain1": {"strain1": 1.0, "strain2": 0.5},
+        "strain2": {"strain1": 0.5, "strain2": 1.0},
+    }
+    with pytest.raises(ValidationError):
+        config.TransmissionParams(
+            strains=strains, strain_interactions=strain_interactions
+        )
+    # test again but with different field missing
+    strains = [
+        config.Strain(
+            strain_name="strain1",
+            r0=2,
+            infectious_period=5,
+            vaccine_efficacy={0: 0.8, 1: 0.9},
+        ),
+        config.Strain(
+            strain_name="strain2", r0=2, infectious_period=5
+        ),  # missing vaccine_efficacy
+    ]
+    with pytest.raises(ValidationError):
+        config.TransmissionParams(
+            strains=strains, strain_interactions=strain_interactions
+        )
+
+
+def test_solver_params():
+    """Test that the SolverParams config is valid."""
+    try:
+        solver_params = config.SolverParams(
+            max_steps=1000,
+            ode_solver_rel_tolerance=1e-6,
+            ode_solver_abs_tolerance=1e-9,
+        )
+    except ValidationError as e:
+        pytest.fail(f"SolverParams raised ValidationError unexpectedly: {e}")
+    assert solver_params.max_steps == 1000
+    assert solver_params.ode_solver_rel_tolerance == 1e-6
+    assert solver_params.ode_solver_abs_tolerance == 1e-9
+
+
+def test_solver_params_invalid():
+    """Test that the SolverParams raises an error for invalid parameters."""
+    with pytest.raises(ValidationError):
+        config.SolverParams(
+            max_steps=-1000,  # Invalid negative max_steps
+            ode_solver_rel_tolerance=1e-6,
+            ode_solver_abs_tolerance=1e-9,
+        )
+    with pytest.raises(ValidationError):
+        config.SolverParams(
+            max_steps=1000,
+            ode_solver_rel_tolerance=-1e-6,  # Invalid negative tolerance
+            ode_solver_abs_tolerance=1e-9,
+        )
