c608a96fcb0139b8cd6fefa5f15de80024e21b12
Nick Adams <45801555+Nick-P-Adams@users.noreply.github.com>
2025-06-24 10:50:04 -0500

    Adding Sphinx API Documentation (#441)
    * initial commit + initial sphinx configuration
    
    * commented out the exclusion of docs/ in git ignore file. kept the docs/build exclusion to remove the fully built documentation. keeping docs/source to preserve the sphinx configuration files and the templates.
    
    * adding generate-docs github workflow
    
    * updating generate-docs.yaml to get insights on dynode module not found error
    
    * removing pre-generated _autosummary .rst files
    
    * pushing docs/build folder and updated generate-docs.yaml inline with an example on sphinx's website
    
    * adding python and poetry install steps back in
    
    * adding python and poetry install steps back in
    
    * adding requirements.txt to docs/
    
    * removing ammaraskar step from workflow
    
    * Testing deploy step for docs
    
    * updating generate-docs.yaml
    
    * updating generate-docs.yaml
    
    * changing upload artifact name
    
    * trying fix on the Build HTML step
    
    * trying a fix for deploy pages step
    
    * trying peaceiris/actions-gh-pages@v3 for deploy step
    
    * removing id from deploy step
    
    * adding permissions contents write under jobs and changing deploy step back to actions/deploy-pages@v4
    
    * changing contents permission before jobs
    
    * debugging sphinx build dynode module not found error
    
    * debugging sphinx build dynode module not found error
    
    * removing sys.path.insert from conf.py
    
    * adding ls to build HTML step for debugging
    
    * attempting a different sys.path.insert
    
    * trying another sys.path.insert
    
    * updating workflow to trigger on pull_request to test
    
    * updating workflow to trigger on pull_request to test
    
    * added pyproject.toml parsing to conf.py to update info like version, authors, and license automatically
    
    * in conf.py joining authors list on empty string
    
    * changing pull request branch back to main
    
    * commenting out exclude_patterns in conf.py
    
    * adding development branch to the pull_request trigger on generate-docs.yaml
    
    * removing development branch as pull-request trigger on generate-docs.yaml

---
+++ docs/source/conf.py
@@ -0,0 +1,53 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+
+import tomli
+
+sys.path.insert(0, os.path.abspath("../../src"))
+
+with open("../../pyproject.toml", "rb") as file:
+    pyproject_data = tomli.load(file)
+
+project = "DynODE"
+copyright = pyproject_data["tool"]["poetry"]["license"]
+author = "".join(pyproject_data["tool"]["poetry"]["authors"])
+release = pyproject_data["tool"]["poetry"]["version"]
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+]
+autosummary_generate = True
+intersphinx_mapping = {
+    "pydantic": ("https://docs.pydantic.dev/latest", None),
+}
+
+templates_path = ["_templates"]
+# exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_book_theme"
+html_theme_options = {
+    "repository_url": "https://github.com/CDCgov/DynODE",
+    "use_repository_button": True,
+    "show_navbar_depth": 2,
+}
+html_static_path = ["_static"]
+html_css_files = [
+    "style.css",
+]
---
+++ docs/source/index.rst
@@ -0,0 +1,27 @@
+.. DynODE documentation master file, created by
+   sphinx-quickstart on Wed Jun  4 13:38:38 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+DynODE API Documentation
+========================
+
+/* Below API Documentation Description TBD /*
+
+This repository is for the design and implementation of the DynODE framework for disease Scenario forecasting, built by the Scenarios team within CFA-Predict.
+
+Currently, we aim to use this code to forecast different disease tranmission scenarios with a compartmental mechanistic ODE model.
+We aim to provide enough flexibility for the code users to explore a variety of scenarios, but also making certain design decisions that allow for fast computation and fitting as well as code readability.
+
+.. autosummary::
+   :toctree: _autosummary
+   :template: module.rst
+   :recursive:
+
+   dynode
+
+Index
+=====
+
+* :ref:`genindex`
+* :ref:`modindex`
