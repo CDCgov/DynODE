c18a6a0ea1e0bed8efade3c985aa51ca91b20639
Nicholas Adams <kzz0@AZ1CFA1VSWL-18.ext.cdc.gov>
2025-09-08 18:11:46 +0000

    removing commented out lines in CompartmentalModel class. Added print statements for debugging.

--- src/dynode/config/compartmental_model.py
+++ src/dynode/config/compartmental_model.py
@@ -4,25 +4,25 @@ from pydantic import (
 )
 
 from .parameter_set import ParameterSet
-
-# from .sample import sample_then_resolve
-# from .simulation_config import SimulationConfig
+from .sample import sample_then_resolve
+from .simulation_config import SimulationConfig
 
 
 class CompartmentalModel(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True, extra="allow")
 
     shared_parameters: ParameterSet  # add Pydantic Field to class attributes
-    # configs: dict[int, SimulationConfig]
+    configs: dict[int, SimulationConfig]
 
-    #    def model_post_init(self, __context) -> None:
-    #        self.shared_parameters = sample_then_resolve(self.shared_parameters)
-    #
-    #        for _, config in self.configs.items():
-    #            config.inject_parameters(parameter_set=self.shared_parameters)
-    #            print(type(config))
-    #            print(type(self.shared_parameters))
-    #            config.sample_then_resolve_parameters()
+    def model_post_init(self, __context) -> None:
+        print(type(sample_then_resolve(self.shared_parameters)))
+        self.shared_parameters = sample_then_resolve(self.shared_parameters)
+        print(type(self.shared_parameters))
+        for _, config in self.configs.items():
+            config.inject_parameters(parameter_set=self.shared_parameters)
+            print(type(config))
+            print(type(self.shared_parameters))
+            config.sample_then_resolve_parameters()
 
     def numpyro_model(self, **kwargs):
         """User must implement this method to define the NumPyro model."""
