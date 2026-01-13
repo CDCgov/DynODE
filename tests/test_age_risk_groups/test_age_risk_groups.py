b0dbd2a85ec60a4e542cba386d2ed31f03720d12
Fuhan Yang <ab32@cdc.gov>
2026-01-12 23:12:27 -0500

    check values

--- tests/test_age_risk_groups/test_age_risk_groups.py
+++ tests/test_age_risk_groups/test_age_risk_groups.py
@@ -3,12 +3,13 @@ import pytest
 
 from dynode.config import Dimension
 from dynode.config.bins import AgeBin, Bin
-from examples.sir_age_risk_stratified import get_config, get_odeparams
-
-## check shape ##
+from examples.sir_age_risk_stratified import (
+    get_config,
+    get_odeparams,
+)
 
 pytestmark = pytest.mark.parametrize(
-    "inputs, expected_init_prop_shape, expected_cm_shape",
+    "inputs, expected_init_prop_shape, expected_cm_shape, expected_init_prop, expected_cm",
     [
         (
             {
@@ -25,6 +26,8 @@ pytestmark = pytest.mark.parametrize(
             },
             (1, 1),
             (1, 1, 1, 1),
+            jnp.array([[1.0]]),
+            jnp.array([[[[1.0]]]]),
         ),
         (
             {
@@ -48,6 +51,14 @@ pytestmark = pytest.mark.parametrize(
             },
             (3, 1),
             (3, 1, 3, 1),
+            jnp.array([[0.7], [0.2], [0.1]]),
+            jnp.array(
+                [
+                    [[[0.7], [0.2], [0.1]]],
+                    [[[0.2], [0.7], [0.1]]],
+                    [[[0.1], [0.1], [0.8]]],
+                ]
+            ),
         ),
         (
             {
@@ -71,6 +82,23 @@ pytestmark = pytest.mark.parametrize(
             },
             (3, 2),
             (3, 2, 3, 2),
+            jnp.array([[0.07, 0.63], [0.12, 0.08], [0.08, 0.02]]),
+            jnp.array(
+                [
+                    [
+                        [[0.49, 0.21], [0.14, 0.06], [0.07, 0.03]],
+                        [[0.21, 0.49], [0.06, 0.14], [0.03, 0.07]],
+                    ],
+                    [
+                        [[0.14, 0.06], [0.49, 0.21], [0.07, 0.03]],
+                        [[0.06, 0.14], [0.21, 0.49], [0.03, 0.07]],
+                    ],
+                    [
+                        [[0.07, 0.03], [0.07, 0.03], [0.56, 0.24]],
+                        [[0.03, 0.07], [0.03, 0.07], [0.24, 0.56]],
+                    ],
+                ]
+            ),
         ),
     ],
 )
@@ -86,13 +114,30 @@ def config_params():
     return config_params
 
 
-def test_shape(
-    config_params, inputs, expected_init_prop_shape, expected_cm_shape
+def test_shape_and_value(
+    config_params,
+    inputs,
+    expected_init_prop_shape,
+    expected_cm_shape,
+    expected_init_prop,
+    expected_cm,
 ):
     config = get_config(config_params | inputs)
 
+    ## check the shape of age_risk_proportion and age_risk_contact matrix ##
     assert (
         config.initializer.get_initial_state()[0].shape
         == expected_init_prop_shape
     )
     assert get_odeparams(config).contact_matrix.shape == expected_cm_shape
+
+    ## check expected values of age_risk_prop and contact matrix ##
+    age_risk_prop = (
+        config.initializer.get_initial_state(s0_prop=1.0, i0_prop=0.0)[0]
+        / config.initializer.population_size
+    )
+
+    assert jnp.all(jnp.abs(age_risk_prop - expected_init_prop) < 1e-6)
+    cm = get_odeparams(config).contact_matrix
+
+    assert jnp.all(jnp.abs(cm - expected_cm) < 1e-6)
