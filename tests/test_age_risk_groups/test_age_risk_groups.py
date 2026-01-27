93446a3afa96f952aa9fe4326b7f0a5973fe235b
Fuhan Yang <ab32@cdc.gov>
2026-01-27 11:16:50 -0500

    update test

--- tests/test_age_risk_groups/test_age_risk_groups.py
+++ tests/test_age_risk_groups/test_age_risk_groups.py
@@ -10,12 +10,14 @@ from examples.sir_age_risk_stratified import (
 
 ## check shape and value of initial proportion and contact matrix ##
 pytestmark = pytest.mark.parametrize(
-    "inputs, expected_init_prop_shape, expected_cm_shape, expected_init_prop, expected_cm",
+    "inputs, expected_init_prop_shape, expected_cm_shape, expected_cm",
     [
         (
             {
                 "age_demographics": jnp.array([1.0]),
                 "risk_prop": jnp.array([[1.0]]),
+                "s0_prop": jnp.array([[0.99]]),
+                "i0_prop": jnp.array([[0.01]]),
                 "age_contact_matrix": jnp.array([[1.0]]),
                 "risk_contact_matrix": jnp.array([[1.0]]),
                 "age_dimension": Dimension(
@@ -27,13 +29,14 @@ pytestmark = pytest.mark.parametrize(
             },
             (1, 1),
             (1, 1, 1, 1),
-            jnp.array([[1.0]]),
             jnp.array([[[[1.0]]]]),
         ),
         (
             {
                 "age_demographics": jnp.array([0.7, 0.2, 0.1]),
                 "risk_prop": jnp.array([[1.0], [1.0], [1.0]]),
+                "s0_prop": jnp.array([[0.99], [0.99], [1.0]]),
+                "i0_prop": jnp.array([[0.01], [0.01], [0.0]]),
                 "age_contact_matrix": jnp.array(
                     [[0.7, 0.2, 0.1], [0.2, 0.7, 0.1], [0.1, 0.1, 0.8]]
                 ),
@@ -52,7 +55,6 @@ pytestmark = pytest.mark.parametrize(
             },
             (3, 1),
             (3, 1, 3, 1),
-            jnp.array([[0.7], [0.2], [0.1]]),
             jnp.array(
                 [
                     [[[0.7], [0.2], [0.1]]],
@@ -65,6 +67,8 @@ pytestmark = pytest.mark.parametrize(
             {
                 "age_demographics": jnp.array([0.7, 0.2, 0.1]),
                 "risk_prop": jnp.array([[0.1, 0.9], [0.6, 0.4], [0.8, 0.2]]),
+                "s0_prop": jnp.array([[0.99, 1.0], [0.99, 0.99], [1.0, 1.0]]),
+                "i0_prop": jnp.array([[0.01, 0.0], [0.01, 0.01], [0.0, 0.0]]),
                 "age_contact_matrix": jnp.array(
                     [[0.7, 0.2, 0.1], [0.2, 0.7, 0.1], [0.1, 0.1, 0.8]]
                 ),
@@ -83,7 +87,6 @@ pytestmark = pytest.mark.parametrize(
             },
             (3, 2),
             (3, 2, 3, 2),
-            jnp.array([[0.07, 0.63], [0.12, 0.08], [0.08, 0.02]]),
             jnp.array(
                 [
                     [
@@ -120,7 +123,6 @@ def test_shape_and_value(
     inputs,
     expected_init_prop_shape,
     expected_cm_shape,
-    expected_init_prop,
     expected_cm,
 ):
     config = get_config(config_params | inputs)
@@ -132,13 +134,5 @@ def test_shape_and_value(
     )
     assert get_odeparams(config).contact_matrix.shape == expected_cm_shape
 
-    ## check expected values of age_risk_prop and contact matrix ##
-    age_risk_prop = (
-        config.initializer.get_initial_state(s0_prop=1.0, i0_prop=0.0)[0]
-        / config.initializer.population_size
-    )
-
-    assert jnp.all(jnp.abs(age_risk_prop - expected_init_prop) < 1e-6)
     cm = get_odeparams(config).contact_matrix
-
     assert jnp.all(jnp.abs(cm - expected_cm) < 1e-6)
